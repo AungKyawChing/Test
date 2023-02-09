bd50f1b7bfa072ca8874be26301cb3d55374b31b
AungKyawChing <122950055+AungKyawChing@users.noreply.github.com>
2023-02-09 12:49:47 +0600

    0.0
    all uploaded

---
+++ js/question.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+        numb:1,
+        question:'Who Created This Quiz App?',
+        answer:'Aung Kyaw Ching',
+        options:[
+            "Foreign Company",
+            "Governmen Of Bangladesh",
+            "Aung Kyaw Ching",
+            "Bangladesh Education Board"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Where is he now?",
+        answer: "Cox's Bazar",
+        options: [
+          "Cox's Bazar",
+          "Cumilla",
+          "Chittagong",
+          "Barishal"
+        ]
+      },
+      {
+        numb: 3,
+        question: "Why this test app Release?",
+        answer: "For testing",
+        options: [
+          "aita nah",
+          "For testing",
+          "bla bla bla",
+          "onno akta correct"
+        ]
+      },
+    
+      {
+        numb: 4,
+        question: "What is the future plan of this project?",
+        answer: "Cover all Mcq Board Question and cover all NCTB Book MCQ Chapterwise",
+        options: [
+          "Hudai kono kaz nai",
+          "Play Football",
+          "Time Pass",
+          "Cover all Mcq Board Question and cover all NCTB Book MCQ Chapterwise"
+        ]
+      },
+
+      {
+        numb: 5,
+        question: "ai app a ki kno partner lagbe?",
+        answer: "Yes, Partner need for add new question",
+        options: [
+          "Nah, Ekai 100",
+          "Yes, Partner need for add new question",
+          "Eka kaz kora valo",
+          "Partner newa vhalo nah"
+        ]
+      },
+]
